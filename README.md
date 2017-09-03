398c395fce7445f1b09ea9e2bec02fdb1dd58ed1
Matt Bagnara <bagnaramatt@gmail.com>
2017-09-03 10:52:36 -0400

    added documentation

---
+++ README.md
@@ -0,0 +1,57 @@
+=Rentcalc
+
+A simple utility to tabulate and generate rent reports. Very useful in room-mate situations where you want to make clear the current bills. As a bonus, it creates a graphical line graph representation of your bills so you can track the monthly costs.
+
+Generation is easy. Simply prepend a listing of bills, one per line. Order is important.
+
+==Requirements
+
+Machine you run the script must:
+* Have a recent version of Ruby
+* Have R
+* Have rinruby gem installed
+
+==Usage
+```
+Usage: rentcalc.rb [options]
+    -f, --file PATH                  Path to bills.txt.
+    -r, --roommates N                Number of roommates to divide bills
+    -h, --help                       Prints this help.
+```
+
+==Bills.txt
+
+This file contains a listing of collected bills in a monthly format.
+
+Example:
+```
+# AUGUST 2017
+2000
+65.55
+79.48
+69.99
+0
+====
+# JULY 2017
+2000
+0
+127.70
+69.99
+266.38
+====
+
+```
+
+```
+# Monthly comment
+RENT VALUE
+GAS VALUE
+ELECTRIC VALUE
+INTERNET VALUE
+COUNTY UTILITIES VALUE
+==== delimeter
+... Next month here
+```
+
+Feel free to add more fields if you need, just update the script and r script to get the pretty chart.
+
--- rentcalc.rb
+++ rentcalc.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/ruby
 # Rentcalc, a simple billing report generator.
 # 
 # Matt Bagnara
@@ -5,57 +6,77 @@
 # 
 # CC-BY-SA
 
-rent=Array.new
-gas=Array.new
-elec=Array.new
-internet=Array.new
-util=Array.new
-total=Array.new
-total2=Array.new
+require 'rinruby'
+require 'optparse'
+require 'pp'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = 'Usage: load-images.rb [options]'
+  opts.on('-f', '--file PATH', 'Path to bills.txt.') { |v| options[:path] = v }
+  opts.on('-r', '--roommates N', Integer, 'Number of roommates to divide bills') { |v| options[:roommates] = v }
+  opts.on('-h', '--help', 'Prints this help.') { puts opts; exit }
+end.parse!
+
+options[:path] != '' ? path = options[:path] : path="./bills.txt"
+options.has_key? :roommates ? roommates = options[:roommates] : roommates=3
+
+rent     = []
+gas      = []
+elec     = []
+internet = []
+util     = []
+total    = []
+total2   = []
 
 num=0
-text=File.open('bills.txt').read
+
+if !File.exists?(path) then
+  printf("Error: File %s does not exist!\n",path)
+  exit
+end
+
+text=File.open(path).read
 text.each_line do |line|
   i=num/6
   val=line.chomp
   if val =~ /^[^#].*$/ then
-    rent[i] = Float(val) if num % 6 == 0
-    gas[i] = Float(val) if num % 6 == 1
-    elec[i] = Float(val) if num % 6 == 2
+    rent[i]     = Float(val) if num % 6 == 0
+    gas[i]      = Float(val) if num % 6 == 1
+    elec[i]     = Float(val) if num % 6 == 2
     internet[i] = Float(val) if num % 6 == 3
-    util[i] = Float(val) if num % 6 == 4
-    total[i] = rent[i] + gas[i] + elec[i] +internet[i] + util[i] if num % 6 == 5
-    total2[i] = total[i] - rent[i] if num % 6 == 5
+    util[i]     = Float(val) if num % 6 == 4
+    total[i]    = rent[i] + gas[i] + elec[i] +internet[i] + util[i] if num % 6 == 5
+    total2[i]   = total[i] - rent[i] if num % 6 == 5
     num+=1
   end
 #break if num > 12
 end
 
 print "Our bill:\n\n\n"
-printf("%1$-30s %2$20s %3$10s\n", "Item", "Previous Month", "Cost")
+printf("%1$-30s %2$20s %3$10s\n"                                          , "Item"         , "Previous Month" , "Cost")
 print("==============================================================\n")
-printf("%1$-30s %2$20.2f %3$10.2f\n", "Rent", rent[1], rent[0])
-printf("%1$-30s %2$20.2f %3$10.2f\n", "Elec", elec[1], elec[0])
-printf("%1$-30s %2$20.2f %3$10.2f\n", "Utilities", util[1], util[0])
-printf("%1$-30s %2$20.2f %3$10.2f\n", "Gas", gas[1], gas[0])
-printf("%1$-30s %2$20.2f %3$10.2f\n", "Internet", internet[1], internet[0])
+printf("%1$-30s %2$20.2f %3$10.2f\n"                                      , "Rent"         , rent[1]          , rent[0])
+printf("%1$-30s %2$20.2f %3$10.2f\n"                                      , "Elec"         , elec[1]          , elec[0])
+printf("%1$-30s %2$20.2f %3$10.2f\n"                                      , "Utilities"    , util[1]          , util[0])
+printf("%1$-30s %2$20.2f %3$10.2f\n"                                      , "Gas"          , gas[1]           , gas[0])
+printf("%1$-30s %2$20.2f %3$10.2f\n"                                      , "Internet"     , internet[1]      , internet[0])
 print "\n\n"
 print("==============================================================\n")
-printf("%1$-51s %2$10.2f\n", "Subtotal", total[0])
-printf("%1$-51s %2$10.2f\n", "Divided by 3", total[0]/3)
+printf("%1$-51s %2$10.2f\n"                                               , "Subtotal"     , total[0])
+printf("%1$-51s %2$10.2f\n"                                               , "Divided by #{roommates}" , total[0]/roommates)
 print("==============================================================\n")
 
-require "rinruby"
 R.quit
 
 r_runtime=RinRuby.new(false)
 
-r_runtime.rent=rent
-r_runtime.elec=elec
-r_runtime.util=util
-r_runtime.gas=gas
-r_runtime.internet=internet
-r_runtime.total=total2
+r_runtime.rent     = rent
+r_runtime.elec     = elec
+r_runtime.util     = util
+r_runtime.gas      = gas
+r_runtime.internet = internet
+r_runtime.total    = total2
 
 procedure=""
 
